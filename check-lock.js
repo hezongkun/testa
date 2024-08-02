1aa4296ed19093c1b6e8469ade78f46216eabff3
大白兔奶头 <hezongkun@qq.com>
2024-08-02 17:44:11 +0800

    Update check-lock.js

--- check-lock.js
+++ check-lock.js
@@ -1,26 +1,20 @@
+/***********************************
 [rewrite_local]
-  
-https?:\/\/www\.gofish\.ren\/v1\/index\/check-lock url script-response-body https://raw.githubusercontent.com/hezongkun/testa/main/check-lock.js
-
+^https://www\.gofish\.ren/v1/index/check-lock url script-response-body https://raw.githubusercontent.com/hezongkun/testa/main/check-lock.js
+ 
 [mitm] 
-
 hostname=www.gofish.ren
+***********************************/
 
 
-
-
-
-
-var test = {
-  "lottery_type": "no_seize_seat",
-  "time": 55000,
-  "start_time": "2024-08-02 15:58:00",
-  "is_lock": 1,
-  "fishing_name": "08.02测试勿买",
-  "fid": "31425",
-  "error": "",
-  "request_time": 50055,
-  "desc": "（普通鱼票）正在放号"
+var body = $response.body
+body = JSON.parse(body)
+if(body.time){
+  body.time = body.time - 10000
+  if(body.time<10000){
+    body.time = 0
+  }
+  $notify('倒计时减10s')
 }
-var body = JSON.stringify(test)
+var body = JSON.stringify(body)
 $done(body)
