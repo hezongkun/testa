2d274b5803103c551b30cb951f6b4fa7b5c6ee84
大白兔奶头 <hezongkun@qq.com>
2024-08-02 17:39:42 +0800

    Update change-btn.js

--- change-btn.js
+++ change-btn.js
@@ -9,11 +9,11 @@ hostname=www.gofish.ren
 
 var body = $response.body
 body = JSON.parse(body)
-body.ticket_list[0].angling_name = '测试123'
 body.ticket_list[0].ticket_site_btn_str = '换位'
 body.ticket_list[0].ticket_site_btn_type = 3
 body.ticket_list[0].position_status = 2
 body.ticket_list[0].status = 3
 body = JSON.stringify(body)
 console.log(body);
+$notify('换位功能开启')
 $done(body)
