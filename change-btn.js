99f8ad1452625311619575a37c51ad6368d87fac
大白兔奶头 <hezongkun@qq.com>
2024-08-02 16:22:47 +0800

    Update change-btn.js

--- change-btn.js
+++ change-btn.js
@@ -9,14 +9,6 @@ https?:\/\/www\.gofish\.ren\/v1\/index\/ticket-list url script-response-body htt
 
 hostname=www.gofish.ren
 
-var body = $response.body
-body = JSON.parse(body)
-body['ticket_list'][0]['angling_name'] =111 
-body.ticket_list[0].ticket_site_btn_str = '换位'
-body.ticket_list[0].ticket_site_btn_type = 3
-body.ticket_list[0].position_status = 2
-body.ticket_list[0].status = 3
-body.ticket_list[0].angling_name +=111 
 
-body = JSON.stringify(body)
+var body = $response.body.replace(/志洋百川台钓/g, 'test')
 $done({ body })
