9ca6897b4a5ca9a4bc95dd5bc5c340b7a7ba98e0
大白兔奶头 <hezongkun@qq.com>
2024-08-02 15:22:13 +0800

    Update and rename change-btn to change-btn.js

---
+++ change-btn.js
@@ -0,0 +1,16 @@
+
+[rewrite_local]
+  
+# > 
+https?:\/\/gofish\.ren\/v1\/index\/ticket-list url script-response-body https://github.com/hezongkun/testa/blob/main/change-btn.js
+
+
+[mitm] 
+
+hostname=www.gofish.ren
+
+var body = $response.body
+body = JSON.parse(body)
+body.ticket_list[0].ticket_site_btn_type = 3
+body = JSON.stringify(body)
+$done({ body })
