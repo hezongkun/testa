a3f36a903a483bfbaa6f4b6c1a0a97bc4acfb4b4
大白兔奶头 <hezongkun@qq.com>
2024-08-02 15:53:10 +0800

    Update check-lock.js

--- check-lock.js
+++ check-lock.js
@@ -14,7 +14,7 @@ hostname=www.gofish.ren
 var test = {
   "lottery_type": "no_seize_seat",
   "time": 55000,
-  "start_time": "2024-08-02 14:00:00",
+  "start_time": "2024-08-02 15:58:00",
   "is_lock": 1,
   "fishing_name": "08.02测试勿买",
   "fid": "31425",
